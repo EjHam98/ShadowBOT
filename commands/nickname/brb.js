06ed9d4f4bd60566906554774d742c441e51e922
EjHam98 <66335818+EjHam98@users.noreply.github.com>
2023-06-29 04:57:41 +0300

    Update brb.js

--- commands/nickname/brb.js
+++ commands/nickname/brb.js
@@ -6,6 +6,26 @@ module.exports = {
 		.setDescription('Add/remove the [BRB] tag'),
 	async execute(interaction) {
 		var tag = " [BRB]"
+		var afktag = " [AFK]"
+		if (interaction.member.nickname.endsWith(afktag)){
+			var newUsername = interaction.member.nickname.slice(0, -tag.length);;
+			try {
+				await interaction.member.setNickname(newUsername);
+			} 
+			catch (error) {
+				console.error(error);
+				await interaction.reply('Failed to update username.');
+			}
+			newUsername = `${interaction.member.nickname}${tag}`;
+			try {
+				await interaction.member.setNickname(newUsername);
+				await interaction.reply(`${interaction.member.nickname} is now BRB.`);
+			} 
+			catch (error) {
+				console.error(error);
+				await interaction.reply('Failed to update username.');
+			}
+		}
 		if (interaction.member.nickname.endsWith(tag)){
 			var newUsername = interaction.member.nickname.slice(0, -tag.length);;
 			try {
@@ -21,7 +41,7 @@ module.exports = {
 			var newUsername = `${interaction.member.nickname}${tag}`;
 			try {
 				await interaction.member.setNickname(newUsername);
-				await interaction.reply(`${interaction.member.nickname} is BRB.`);
+				await interaction.reply(`${interaction.member.nickname} is now BRB.`);
 			} 
 			catch (error) {
 				console.error(error);
